c30a8ce382a290ca9a7652799538a1d9f0d47ee4
PhilippJoerg <philipp.joerg@plan-b-gmbh.com>
2019-04-03 10:43:43 +0200

    added basic webpage structure

--- SuperAmazingShop/src/app/app.module.ts
+++ SuperAmazingShop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ShoppingPageComponent
   ],
   imports: [
     BrowserModule,
