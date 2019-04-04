82939594b603aca1cb963d1387f8b4c145a5abe8
PhilippJoerg <philipp.joerg@plan-b-gmbh.com>
2019-04-04 17:01:25 +0200

    started the shopping page

--- SuperAmazingShop/src/app/home-page/home-page.component.ts
+++ SuperAmazingShop/src/app/home-page/home-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements OnInit {
   constructor() {
+  }
+
+  ngOnInit() {
+    $('.carousel').carousel({interval: 3000});
+
+    const $toggle = $('#toggleslide');
+    $toggle.bind('click', () => {
+        if ($toggle.hasClass('pause')) {
+            $('.carousel').carousel('cycle');
+            $toggle.removeClass('pause');
+        } else {
+            $('.carousel').carousel('pause');
+            $toggle.addClass('pause');
+        }
+    });
   }
 }
